eb953203b998d756a2a1cd60a7ee5829982e6127
Oleksander Nazarchuk <ss.nazarchuk2@gmail.com>
2023-05-01 21:41:05 +0300

    Creating the main activity and activity for creating a new category

---
+++ Client/app/src/main/java/com/example/clientandroid/services/CategoryCreateActivity.java
@@ -0,0 +1,14 @@
+package com.example.clientandroid.services;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CategoryCreateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_category_create);
+    }
+}
---
+++ Client/app/src/main/java/com/example/clientandroid/services/dto/CategoryCreateDTO.java
@@ -0,0 +1,2 @@
+package com.example.clientandroid.services.dto;public class CategoryCreateDTO {
+}
